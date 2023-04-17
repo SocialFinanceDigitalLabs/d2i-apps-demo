a38cf8c60775a2d08d2eff65af991024f1fc6733
franciscobmacedo <franciscobmacedo@users.noreply.github.com>
2023-04-17 19:14:29 +0000

    deploy: 0c8fdd2450231e87b33d12f33ab9495e70707f37

--- D2i_apps.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="D2I Apps")
-st.title("D2I Apps")
-
-st.write("choose an app to run on the sidebar!")
--- main.js
+++
@@ -1,8 +0,0 @@
-const main = async () => {
-  const response = await fetch("./config.json");
-  const config = await response.json();
-  console.log(config);
-  stlite.mount(config, document.getElementById("root"));
-};
-
-main();
