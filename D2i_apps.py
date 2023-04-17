b942e5d91c38252603748c167108b5d2e47f6850
franciscobmacedo <franciscovcbm@gmail.com>
2023-04-17 17:51:24 +0100

    test small config change

--- D2i_apps.py
+++ D2i_apps.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="D2I Apps")
 st.title("D2I Apps")
 
-st.write("choose an app to run on the sidebar")
+st.write("choose an app to run on the sidebar!")
